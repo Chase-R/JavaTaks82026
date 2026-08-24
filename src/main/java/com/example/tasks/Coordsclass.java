352ebcb84ce65bfbacf62b3e750a5036f827fc6b
Chase <Cpreabe@gmail.com>
2026-08-24 13:37:16 -0500

    coordinates

--- src/main/java/com/example/tasks/Coordsclass.java
+++ src/main/java/com/example/tasks/Coordsclass.java
@@ -3,8 +3,11 @@ package com.example.tasks;
 public class Coordsclass {
 
     public static String slope(int x1,int x2,int y1,int y2){
-        String x = String.valueOf((y2-y1)/(x2-x1));
-        return x;
+        if (x2 == x1) {
+            return "undefined";
+        }
+        String slope = String.valueOf((y2-y1)/(x2-x1));
+        return slope;
 
     }
 
